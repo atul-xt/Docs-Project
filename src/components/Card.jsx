09ee31aafa7985a6049a44b328c97ab443dad307
atul-xt <atul.xtt@gmail.com>
2024-03-20 15:23:54 +0530

    fully updated for mobile version

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ function Card({data, reference}) {
   return (
     <motion.div drag dragConstraints={reference} whileDrag={{scale: 1.1}}  className={`relative h-56 w-[175px] z-30 px-4 pt-5 overflow-hidden text-white rounded-[30px] bg-[#1c1b1b]`}>
       <FaRegFileAlt />
-      <div className='h-[130px] mt-3 overflow-y-scroll'>
+      <div className='h-[130px] mt-3 overflow-y-scroll hide'>
       <p className='text-[12px] '>{data.notes}</p>
       </div>
       <div className={`absolute flex items-center justify-center bottom-0 ${data.color} left-0 h-10 w-full`}>
